1100eae92301a6c8c5cddf53eaae01a6606db0a7
ByElection <gonza.zarzabal@gmail.com>
2021-10-22 23:49:52 -0300

    se acomodaron animaciones y relacion de aspecto para aplicar parallax

--- Practico3/js/Bike.js
+++ Practico3/js/Bike.js
@@ -1,6 +1,11 @@
 class Bike extends ObjetoSolido{
-  constructor(posx,posy,w,h) {
-    super(posx,posy,w,h);
+  constructor(posx,posy,w,h,id) {
+    super(70-12*id,16*(id+1),20,21);
+    this.bike = document.querySelectorAll(".bike")[id];
+
+  }
+  prendeMotor(){
+    changeAnimation(this.bike,"motor",0.5,"steps(1) infinite");
   }
 
 }
--- Practico3/js/Main.js
+++ Practico3/js/Main.js
@@ -1,7 +1,11 @@
 let pista;
+let bikes = new Array(4);
 
 function juegoNuevo() {
   pista=new Track();
+  /*for (var i = 0; i < bikes.length; i++) {
+    bikes[i] = new Bike();
+  }*/
   darlargada();
 }
 
@@ -18,6 +22,10 @@ function darlargada(){
   changeAnimation(largada,"darlargada",3,"steps(1) forwards"); //forwards para que frene cuando termina
   let track = document.querySelector("#track");
   changeAnimation(track,"trackmove",20,"linear 3s");
+  let tribuna = document.querySelector("#tribuna");
+  changeAnimation(tribuna,"adornosmove",1500,"linear infinite 3s");
+  let personas = document.querySelector("#personas");
+  changeAnimation(personas,"adornosmove",1500,"linear infinite 3s");
 }
 
 function changeAnimation(element,keyframe,time,params) {
