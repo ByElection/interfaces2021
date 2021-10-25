5adaa052b059510f001de338ddef944ba7698b3a
ByElection <gonza.zarzabal@gmail.com>
2021-10-24 22:44:55 -0300

    se agregaron todas las animaciones que faltaban a la moto

--- Practico3/js/Bike.js
+++ Practico3/js/Bike.js
@@ -20,11 +20,26 @@ class Bike extends ObjetoSolido{
     }else if (this.inclinacion<0 && this.inclinacion>-4) {
       changeAnimation(this.bike,"inclinarinv",4,"steps(4) calc(var(--inclinacion"+this.id+")*1s) 1 both paused");
     }else if (this.inclinacion>=6 || this.inclinacion<=-4) {
-      changeAnimation(this.bike,"caida",1,"steps(4) infinite");
+      this.caerse();
     }
   }
   prendeMotor(delay){
     changeAnimation(this.bike,"motor",0.5,"steps(1) infinite "+delay+"s");
   }
-
+  caerse(){
+    changeAnimation(this.bike,"caida",1,"steps(4) infinite");
+    let bike = this.bike;
+    let biker = document.querySelectorAll(".biker")[this.id];
+    setTimeout(function() {
+      changeAnimation(bike,"mototirada",1,"steps(1) paused");
+      changeAnimation(biker,"bikercaido",3,"steps(10) 1");
+      setTimeout(function() {
+        changeAnimation(bike,"levantada",2,"steps(4)");
+        changeAnimation(biker,"",0,"");
+        setTimeout(function() {
+          changeAnimation(bike,"motor",0.5,"steps(1) infinite");
+        },2000);
+      },3000);
+    },1000);
+  }
 }
